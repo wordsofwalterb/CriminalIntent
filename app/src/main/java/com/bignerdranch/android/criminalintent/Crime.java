dacc9b02f4f27bcf081ffeea01c499c211ee03b7
Brandon Walter <brandonwalter@Brandons-MacBook-Pro-2.local>
2019-01-01 17:51:09 -0600

    Initial commit

---
+++ app/src/main/java/com/bignerdranch/android/criminalintent/Crime.java
@@ -0,0 +1,48 @@
+package com.bignerdranch.android.criminalintent;
+
+import java.util.Date;
+import java.util.UUID;
+
+public class Crime {
+	private UUID mID;
+	private String mTitle;
+	private Date mDate;
+	private boolean mSolved;
+	
+	public Crime(){
+		mID = UUID.randomUUID();
+		mDate = new Date();
+	}
+	
+	public UUID getID() {
+		return mID;
+	}
+	
+	public void setID(UUID ID) {
+		mID = ID;
+	}
+	
+	public String getTitle() {
+		return mTitle;
+	}
+	
+	public void setTitle(String title) {
+		mTitle = title;
+	}
+	
+	public Date getDate() {
+		return mDate;
+	}
+	
+	public void setDate(Date date) {
+		mDate = date;
+	}
+	
+	public boolean isSolved() {
+		return mSolved;
+	}
+	
+	public void setSolved(boolean solved) {
+		mSolved = solved;
+	}
+}
